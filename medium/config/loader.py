b5440b8c046cd0c840a7beac3bda81cf219ab5c8
Wasserstoff <admin@germanhydrogen.org>
2021-01-25 18:15:39 +0100

    Version 4.2.1 Updated config loader

--- medium/config/loader.py
+++ medium/config/loader.py
@@ -3,15 +3,15 @@ import yaml
 
 path = os.path.dirname(os.path.abspath(__file__))
 
-if os.path.isfile(path + '/config.yml'):
-    with open(path + "/config.yml", 'r') as ymlfile:
+if os.path.isfile(os.path.join(path, '/config.yml')):
+    with open(os.path.join(path, '/config.yml'), 'r') as ymlfile:
         cfg = yaml.safe_load(ymlfile)
 else:
-    with open(path + "/default.yml", 'r') as ymlfile:
+    with open(os.path.join(path, "/default.yml"), 'r') as ymlfile:
         cfg = yaml.safe_load(ymlfile)
 
-if os.path.isfile(path + f'/{cfg["language"]}.yml'):
-    with open(path + f'/{cfg["language"]}.yml') as ymlfile:
+if os.path.isfile(os.path.join(path, f'/{cfg["language"]}.yml')):
+    with open(os.path.join(path, f'/{cfg["language"]}.yml')) as ymlfile:
         lang = yaml.safe_load(ymlfile)
 else:
     print("Language File missing")
